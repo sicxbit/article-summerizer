e9b469f5264f6c60b05bc68ec2039a1a802f87db
Arunjith T S <arunjith.unofficial@gmail.com>
2023-04-22 16:14:37 +0530

    initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,36 @@
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header
+      className="w-full flex 
+    justify-center items-center flex-col"
+    >
+      <nav
+        className="flex justify-between
+         items-center w-full mb-10 pt-10"
+      >
+        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => {
+            window.open(`https://gitlab.com/arunjith.official/summerizer`);
+          }}
+          className="black_btn"
+        >
+          Gitlab
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summerize articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">Open AI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        simplify your articles by summize an open-source article summerizer that
+        transforms leangthy articles into clear and consise summeries
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
